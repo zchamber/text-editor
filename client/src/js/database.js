8a0820e00579f481977edbb5e3d7a0aef932104f
Monkey D. Luffy <zachchamber07@gmail.com>
2024-02-09 19:34:38 -0700

    console log

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log('PUT to the database');
   const db = await initdb();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -23,6 +24,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  console.log('GET from the database');
   const db = await initdb();
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
